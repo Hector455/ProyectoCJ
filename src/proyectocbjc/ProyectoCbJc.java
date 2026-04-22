89f91d4af8ac246276c852ea66d73cc05c07ee0e
Hector455 <america.12.tano@gmail.com>
2026-04-22 12:40:58 -0600

    Primer commit de mi proyecto

---
+++ src/proyectocbjc/ProyectoCbJc.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectocbjc;
+
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ProyectoCbJc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       InterBonita pantallita = new InterBonita();
+       pantallita.setVisible(true);
+      
+    }
+    
+    
+}
